9528aecc54c357fcca0ce16b3b0b1a3c295a433f
haji9202 <hayun9202@gmail.com>
2024-07-18 22:43:18 +0900

    fix: fix xml formatter test

--- tests/test_lunar_log.cpp
+++ tests/test_lunar_log.cpp
@@ -20,7 +20,7 @@ protected:
         std::vector<std::string> filesToRemove = {
             "test_log.txt", "level_test_log.txt", "rate_limit_test_log.txt",
             "escaped_brackets_test.txt", "test_log1.txt", "test_log2.txt",
-            "validation_test_log.txt", "custom_formatter_log.txt", "json_formatter_log.txt"
+            "validation_test_log.txt", "custom_formatter_log.txt", "json_formatter_log.txt", "xml_formatter_log.txt"
         };
 
         for (const auto &filename: filesToRemove) {
@@ -204,24 +204,16 @@ TEST_F(LunarLogTest, JsonFormatter) {
 
 TEST_F(LunarLogTest, XmlFormatter) {
     minta::LunarLog logger(minta::LogLevel::INFO);
-    logger.addSink<minta::FileSink, minta::XmlFormatter>("json_formatter_log.txt");
+    logger.addSink<minta::FileSink, minta::XmlFormatter>("xml_formatter_log.txt");
 
     logger.info("User {username} logged in from {ip}", "alice", "192.168.1.1");
 
-    waitForFileContent("json_formatter_log.txt");
-    std::string logContent = readLogFile("json_formatter_log.txt");
+    waitForFileContent("xml_formatter_log.txt");
+    std::string logContent = readLogFile("xml_formatter_log.txt");
 
-    if (!logContent.empty() && logContent.back() == '\n') {
-        logContent.pop_back();
-    }
-
-    EXPECT_TRUE(logContent.find(R"("level":"INFO")") != std::string::npos);
-    EXPECT_TRUE(logContent.find(R"("message":"User alice logged in from 192.168.1.1")") != std::string::npos);
-    EXPECT_TRUE(logContent.find(R"("timestamp":"2024-07-18)") != std::string::npos);  // Check for date part
-    EXPECT_TRUE(logContent.find(R"("username":"alice")") != std::string::npos);
-    EXPECT_TRUE(logContent.find(R"("ip":"192.168.1.1")") != std::string::npos);
-
-    EXPECT_EQ(logContent.front(), '{');
-    EXPECT_EQ(logContent.back(), '}');
-    EXPECT_EQ(std::count(logContent.begin(), logContent.end(), '"'), 20);
+    EXPECT_TRUE(logContent.find("<level>INFO</level>") != std::string::npos);
+    EXPECT_TRUE(logContent.find("<message>User alice logged in from 192.168.1.1</message>") != std::string::npos);
+    EXPECT_TRUE(logContent.find("<timestamp>2024-07-18") != std::string::npos);
+    EXPECT_TRUE(logContent.find("<argument name=\"username\">alice") != std::string::npos);
+    EXPECT_TRUE(logContent.find("<argument name=\"ip\">192.168.1.1") != std::string::npos);
 }

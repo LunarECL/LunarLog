4366e0f06f3e16b660eb8205a880c55b2adc592a
LunarECL <38317983+LunarECL@users.noreply.github.com>
2026-02-18 15:16:48 +0900

    fix: undef ERROR macro in log_level.hpp to prevent Windows.h conflict

--- include/lunar_log/core/log_level.hpp
+++ include/lunar_log/core/log_level.hpp
@@ -1,6 +1,12 @@
 #ifndef LUNAR_LOG_LEVEL_HPP
 #define LUNAR_LOG_LEVEL_HPP
 
+// Windows.h defines ERROR as 0, which conflicts with the ERROR enum member.
+// Undefine it here so LogLevel::ERROR is always usable regardless of include order.
+#ifdef ERROR
+#undef ERROR
+#endif
+
 namespace minta {
     enum class LogLevel {
         TRACE,

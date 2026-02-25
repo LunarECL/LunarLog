4a61824fc2a3e4ae63c5ae52bd231534338600f5
LunarECL <38317983+LunarECL@users.noreply.github.com>
2026-02-25 19:34:07 +0900

    fix vcpkg portfile: install usage file

--- ports/lunarlog/portfile.cmake
+++ ports/lunarlog/portfile.cmake
@@ -20,4 +20,5 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/LunarLog)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
